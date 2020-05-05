2210de07237abed544cc89b75288c035a5997327
kPatch <irvsteve@gmail.com>
2020-05-05 01:05:21 -0400

    Change project to ReactJS project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    // this.state = {
+    //   value: ""
+    // };
+  }
+
+  render() {
+    return (
+      <h1>Hello Ming!</h1>
+    );
+  }
+}
+
+export default App;
